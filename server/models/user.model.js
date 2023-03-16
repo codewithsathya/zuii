48309deeeea3e666a81908f979a605ba605312f9
Sathiya Seelan <codewithsathya@gmail.com>
2023-03-16 15:14:10 +0530

    Add user schema

---
+++ server/models/user.model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    profilePic: {
+        type: String,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Invalid email')
+            }
+        }
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    orders: {
+        type: [mongoose.Schema.ObjectId],
+        ref: "Order"
+    }
+}, { timings: true });
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
