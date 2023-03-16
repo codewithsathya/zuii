1f2dd02a0d8c23e9c0006934bb978e7715f0ec38
Sathiya Seelan <codewithsathya@gmail.com>
2023-03-16 18:36:54 +0530

    Create order controller

--- server/app.js
+++ server/app.js
@@ -28,14 +28,13 @@ app.use("*", cors());
 app.use("/api", routes);
 
 app.use((err, req, res, next) => {
-  const status = err.status || 500;
-  const message = err.message || "Something went wrong!";
-
-  return res.status(status).json({
-    success: false,
-    status,
-    message,
-  });
+    const status = err.status || 500;
+    const message = err.message || "Something went wrong!";
+    return res.status(status).json({
+        success: false,
+        status,
+        message,
+    });
 });
 
 // const dirname = path.resolve("..")
