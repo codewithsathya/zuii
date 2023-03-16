b31e66d296a46473304a8bb00524fc2eda05f5d0
Charan Sai <acs13@iitbbs.ac.in>
2023-03-16 15:59:44 +0530

    modified history component

--- client/src/pages/Test.jsx
+++ client/src/pages/Test.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
-import Map from '../components/Map';
+import React from "react";
+import Table from "../components/table";
+import { MDBContainer, MDBTypography, MDBBtn } from 'mdb-react-ui-kit';
 
 const Test = () => {
-  return <Map />;
+  return( 
+    <>
+    <br />
+    <br />
+     <div className="text-center">
+    <MDBBtn>Book</MDBBtn>
+    </div>
+    <br />
+    <br />
+      <MDBContainer breakpoint="sm"><MDBTypography tag='h2'>History</MDBTypography></MDBContainer>
+      <br />
+      
+     <Table />
+    </>
+    );
 };
 
 export default Test;
+
