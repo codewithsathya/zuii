be7d09b9ce7667d30d2ead90638ae3c7ad20aca9
Nagulapally-Anudeep <nanudeep408@gmail.com>
2023-03-16 13:42:59 +0530

    added pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import OrderHistory from "./pages/OrderHistory";
+import Requests from "./pages/Requests";
+import Test from "./pages/Test";
 
 function App() {
-  return <div className="App">Zuii</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="history" element={<OrderHistory />} />
+        <Route path="requests" element={<Requests />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import store from "./store";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <GoogleOAuthProvider clientId="216168406592-64kfuc404f6apspc4l9pjo9ub6b1j9fq.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
+  </Provider>
+);
