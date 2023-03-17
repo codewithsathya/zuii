cc7c6803412e8d2225b1640be733bbf0737b0f33
Nagulapally-Anudeep <nanudeep408@gmail.com>
2023-03-17 16:24:52 +0530

    added admin table functionality

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
-import NavBar from "../components/NavBar";
 import Table from "../components/Table";
 import { MDBContainer, MDBTypography, MDBBtn } from "mdb-react-ui-kit";
 import { useSelector, useDispatch } from "react-redux";
 import Typography from "@mui/material/Typography";
 import { getOrderList, getRequests } from "../actions/orders";
 import AdminTable from "../components/AdminTable";
+import LayoutWrapper from "../components/LayoutWrapper";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -20,27 +20,28 @@ const Home = () => {
   }, [user, dispatch]);
 
   return (
-    <div>
-      <NavBar />
-      <br />
-      <br />
-      <div className="text-center">
-        <MDBBtn>Book</MDBBtn>
+    <LayoutWrapper>
+      <div className="mx-auto h-full w-full max-w-[1300px] items-center justify-center">
+        <br />
+        <br />
+        <div className="text-center">
+          <MDBBtn>Book</MDBBtn>
+        </div>
+        <br />
+        <br />
+        <MDBContainer breakpoint="sm">
+          <MDBTypography tag="h2">History</MDBTypography>
+        </MDBContainer>
+        <br />
+        <MDBContainer breakpoint="sm">
+          {!user && (
+            <Typography>You need to login to view your history</Typography>
+          )}
+          {user && !user.isAdmin && <Table />}
+          {user && user.isAdmin && <AdminTable />}
+        </MDBContainer>
       </div>
-      <br />
-      <br />
-      <MDBContainer breakpoint="sm">
-        <MDBTypography tag="h2">History</MDBTypography>
-      </MDBContainer>
-      <br />
-      <MDBContainer breakpoint="sm">
-        {!user && (
-          <Typography>You need to login to view your history</Typography>
-        )}
-        {user && !user.isAdmin && <Table />}
-        {user && user.isAdmin && <AdminTable />}
-      </MDBContainer>
-    </div>
+    </LayoutWrapper>
   );
 };
 
