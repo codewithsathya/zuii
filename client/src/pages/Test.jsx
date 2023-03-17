2b9f27187c1e0f3ae7343d8f24af6431aa94088e
Nagulapally-Anudeep <nanudeep408@gmail.com>
2023-03-17 23:52:34 +0530

    fixed conflict

--- client/src/pages/Test.jsx
+++ client/src/pages/Test.jsx
@@ -1,38 +1,5 @@
 import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
-<<<<<<< Updated upstream
-import { useParams } from "react-router-dom";
-import Map from "../components/Map";
-
-const Test = () => {
-  const [location, setLocation] = useState(null);
-  const { id } = useParams();
-
-  useEffect(() => {
-    const socket = io.connect("http://localhost:3000");
-    socket.emit("setup", { id });
-    socket.on("connected", () => {
-      console.log("User successfully connected");
-    });
-    socket.on("failed", () => {
-      console.log("Connection failed");
-    });
-    socket.on("locationupdate", (...args) => {
-      console.log(args);
-    });
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
-
-  return (
-    <>
-      <Map />
-    </>
-  );
-};
-
-=======
 import { useParams, useSearchParams } from "react-router-dom";
 
 const SERVER_ROUTE = "http://localhost:3000/";
@@ -73,5 +40,4 @@ const Test = () => {
   );
 };
 
->>>>>>> Stashed changes
 export default Test;
