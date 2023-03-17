bac71ea717fc4f8543ac78d9aa0b5998cab763e8
MdNaseerHussain <mdnajju013@gmail.com>
2023-03-18 03:45:12 +0530

    Master map socket

---
+++ client/src/pages/MasterMap.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+
+const droneIcon = Leaflet.icon({
+  iconUrl:
+    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAA3QAAAN0BcFOiBwAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAHiSURBVFiF7Ze9TgJREIW/a0JDXDDxTahsLOyESIwdsSA8nbWGzgQaUEwIJPIEvoANhY2JY8Hgjpe7y2UFtWCSaXbPnHN25/46ESErnHNl4AY4FpHTTGC4dgC8Atci8paFO1gj3gUugfdNxDXetbarXOEQka8EHFAHGkAPEGAMVC0uJoGq1opyNZTbfcN54kMtWOYEONpU3HAeKYflHFoTFlz3gALUi4rH8tox4EIdWtvp9RHiSLXUZZm05zYHwOEPvv5QOXzeHlBezkAr/gS0gY4pLGTCEx8oZ5t0TPRUm1sjXvUIhkVMeOJDW8tiYC5nxy0K7BOYakACPIRMaB/PNF2G+AOQZEzRPjCI+ZoEeDQmEuAKeDY9fdZniRF/DImv8Ef+0gowUuJ5YFCJ924EVKK4N+hrBZjliC9zFivurwO5ISJzYBoBnSo2KqIN7Cr2BvYGog045ypALQJaU2xc/PuFiF0vxfxsM7rQLL4ZkW7HY8z5j1/cju2BZMLiDHfO9g8k58r9/UCi4L87khlwKwDsFBU3vJ0Abyu0G76wGh+BZ5tGiCPV8tzee053cTG5z10HgBMW7dj21awFnKzgcgjswOwXMNDHG3ChzNwLZHGlbgJ3QCm+5V9R0tqm5FzPPwErm9VqClDZ8wAAAABJRU5ErkJggg==",
+});
+
+export default function MasterMap() {
+  const token = JSON.parse(localStorage.getItem("profile")).token;
+
+  useEffect(() => {
+    const socket = io.connect("http://localhost:3000");
+    socket.emit("setupAdmin", { token });
+    socket.on("connected", () => {
+      console.log("User successfully connected");
+      socket.emit("get-all-locations");
+    });
+    socket.on("failed", () => {
+      console.log("Connection failed");
+    });
+    socket.on("update-locations", (...args) => {
+      console.log(args);
+    });
+    socket.on("disconnect", () => {
+      socket.disconnect();
+    });
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  return <div>MasterMap</div>;
+}
